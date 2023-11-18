54a47fb4cb1a7fbedf2f30f9e5c25699aeb5ad76
dwi viki p <60940622+Bla-t@users.noreply.github.com>
2023-11-18 10:23:42 +0700

    Update config.php
    hehe.?

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('BASEURL', 'https://bst-ekspres.com');
+define('BASEURL', '');
 
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'u1732330');
-define('DB_PASS', 'dmNz78424325');
-define('DB_NAME', 'u1732330_bst_d4ata_0k');
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
