c1ff0e95ff632f56913397c443e19633b495719c
dwi viki p <60940622+Bla-t@users.noreply.github.com>
2023-11-18 10:00:54 +0700

    first commit config

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('BASEURL', 'http://localhost/web_app');
+define('BASEURL', 'https://bst-ekspres.com');
 
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'web_app');
+define('DB_USER', 'u1732330');
+define('DB_PASS', 'dmNz78424325');
+define('DB_NAME', 'u1732330_bst_d4ata_0k');
